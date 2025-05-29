dc75bd6f05b47cc2c43e8223f050a48efc957c63
LENOVO <LENOVO@DESKTOP-BKGEO4N.mshome.net>
2025-05-29 15:42:04 +0700

    bersih bersih

--- src/smart/restok.java
+++ src/smart/restok.java
@@ -4,7 +4,6 @@
  */
 package smart;
 
-import Config.Session;
 import com.formdev.flatlaf.FlatLightLaf;
 import java.awt.Color;
 import java.awt.Component;
@@ -14,7 +13,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import static javax.management.Query.value;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -25,10 +23,6 @@ import javax.swing.table.DefaultTableModel;
 import popup.dataexpired;
 import popup.datasupplier;
 import popup.datareturn;
-import popup.tambahbarang;
-import popup.tambahsupplier;
-import popup.inputreturn;
-import popup.tambahkaryawan;
 import popup.validasitambahbarang;
 
 /**
@@ -36,14 +30,15 @@ import popup.validasitambahbarang;
  * @author Muhammad Shobri
  */
 public class restok extends javax.swing.JFrame {
- private Connection conn;
+
+    private Connection conn;
     private static final String DB_URL = "jdbc:mysql://localhost:3306/smart";
-    private static final String DB_USER = "root"; // sesuaikan dengan username DB Anda
-    private static final String DB_PASS = ""; // sesuaikan dengan password DB Anda
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
 
     public restok() {
         initComponents();
-        
+
         // Custom header table
         javax.swing.table.JTableHeader header = tablerestock.getTableHeader();
         header.setDefaultRenderer(new DefaultTableCellRenderer() {
@@ -51,58 +46,53 @@ public class restok extends javax.swing.JFrame {
             public Component getTableCellRendererComponent(JTable table, Object value,
                     boolean isSelected, boolean hasFocus, int row, int column) {
                 super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-                
+
                 setBackground(Color.BLACK); // Warna background header
                 setForeground(Color.WHITE); // Warna teks putih
                 setFont(new Font("Segoe UI", Font.BOLD, 12)); // Font lebih tebal
                 setHorizontalAlignment(JLabel.CENTER); // Posisi teks di tengah
-                
+
                 return this;
             }
         });
-        
+
         makeButtonTransparent(dashboard);
         makeButtonTransparent(transaksi);
         makeButtonTransparent(laporan);
         makeButtonTransparent(karyawan);
-     
         makeButtonTransparent(inputbarang);
-      
         makeButtonTransparent(datasupplier);
         makeButtonTransparent(datareturn);
         makeButtonTransparent(dataexpired);
-     
-        
-        // Koneksi database dan load data
         connectToDatabase();
         loadRestockData();
         tablerestock.setDefaultEditor(Object.class, null);
-    
+
     }
-    
-private void connectToDatabase() {
+
+    private void connectToDatabase() {
         try {
             conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(this, "Gagal terhubung ke database: " + e.getMessage(), 
+            JOptionPane.showMessageDialog(this, "Gagal terhubung ke database: " + e.getMessage(),
                     "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     private void loadRestockData() {
         DefaultTableModel model = (DefaultTableModel) tablerestock.getModel();
         model.setRowCount(0); // Kosongkan tabel terlebih dahulu
-        
+
         try {
             // Query untuk mengambil data produk yang perlu restok
-            String query = "SELECT p.id_produk, p.nama_produk, p.harga, p.stok, s.nama_supplier, p.kategori " +
-                          "FROM produk p " +
-                          "LEFT JOIN supplier s ON p.id_supplier = s.id_supplier " +
-                          "WHERE p.stok < 5"; // Ambang batas restok
-            
+            String query = "SELECT p.id_produk, p.nama_produk, p.harga, p.stok, s.nama_supplier, p.kategori "
+                    + "FROM produk p "
+                    + "LEFT JOIN supplier s ON p.id_supplier = s.id_supplier "
+                    + "WHERE p.stok < 5"; // Ambang batas restok
+
             PreparedStatement pst = conn.prepareStatement(query);
             ResultSet rs = pst.executeQuery();
-            
+
             int no = 1;
             while (rs.next()) {
                 String idProduk = rs.getString("id_produk");
@@ -112,11 +102,11 @@ private void connectToDatabase() {
                 int jumlah = rs.getInt("stok");
                 String kategori = rs.getString("kategori");
                 String supplier = rs.getString("nama_supplier");
-                
+
                 if (supplier == null) {
                     supplier = "Tidak ada supplier";
                 }
-                
+
                 model.addRow(new Object[]{
                     no++,
                     namaBarang,
@@ -127,22 +117,21 @@ private void connectToDatabase() {
                     supplier
                 });
             }
-            
+
             rs.close();
             pst.close();
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(this, "Gagal memuat data restok: " + e.getMessage(), 
+            JOptionPane.showMessageDialog(this, "Gagal memuat data restok: " + e.getMessage(),
                     "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     private void makeButtonTransparent(JButton button) {
         button.setOpaque(false);
         button.setContentAreaFilled(false);
         button.setBorderPainted(false);
     }
 
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -252,49 +241,49 @@ private void connectToDatabase() {
 
     private void dashboardActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dashboardActionPerformed
         // TODO add your handling code here:
-          new dashboard().setVisible(true);
-        this.setVisible(false);  
+        new dashboard().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_dashboardActionPerformed
 
     private void transaksiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_transaksiActionPerformed
         // TODO add your handling code here:
-          new transaksi().setVisible(true);
-        this.setVisible(false);  
+        new transaksi().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_transaksiActionPerformed
 
     private void laporanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_laporanActionPerformed
         // TODO add your handling code here:
-          new laporanpenjualan().setVisible(true);
-        this.setVisible(false);  
+        new laporanpenjualan().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_laporanActionPerformed
 
     private void karyawanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_karyawanActionPerformed
         // TODO add your handling code here:
-          new karyawan().setVisible(true);
-        this.setVisible(false);  
+        new karyawan().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_karyawanActionPerformed
 
     private void inputbarangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_inputbarangActionPerformed
         validasitambahbarang popup = new validasitambahbarang();
-                popup.setVisible(true);  
+        popup.setVisible(true);
     }//GEN-LAST:event_inputbarangActionPerformed
 
     private void datasupplierActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_datasupplierActionPerformed
         // TODO add your handling code here:
-         new datasupplier().setVisible(true);
-        this.setVisible(false);  
+        new datasupplier().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_datasupplierActionPerformed
 
     private void dataexpiredActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_dataexpiredActionPerformed
         // TODO add your handling code here:
-         new dataexpired().setVisible(true);
-        this.setVisible(false);  
+        new dataexpired().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_dataexpiredActionPerformed
 
     private void datareturnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_datareturnActionPerformed
         // TODO add your handling code here:
-         new datareturn().setVisible(true);
-        this.setVisible(false);  
+        new datareturn().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_datareturnActionPerformed
 
     /**
@@ -324,7 +313,7 @@ private void connectToDatabase() {
         }
         //</editor-fold>
         //</editor-fold>
-FlatLightLaf.setup();
+        FlatLightLaf.setup();
         UIManager.put("TableHeader.background", Color.BLACK);
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -347,6 +336,5 @@ FlatLightLaf.setup();
     private javax.swing.JTable tablerestock;
     private javax.swing.JButton transaksi;
     // End of variables declaration//GEN-END:variables
-
 
 }
